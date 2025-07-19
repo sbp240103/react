eaedc6e76fa9c1062fc972f8246abdea82ec3f12
Pushkar Singh <rbt4209211@gmail.com>
2025-07-19 23:19:39 +0530

    updated code

--- 01Counter/src/App.jsx
+++ 01Counter/src/App.jsx
@@ -3,8 +3,14 @@ function App() {
   let [counter, setCounter] = useState(10);
   const addValue = () => {
     if(counter <20) setCounter(counter + 1);
+    if(counter <20) setCounter(counter + 1);
+    if(counter <20) setCounter(counter + 1);
+    if(counter <20) setCounter(counter + 1);
   }
   const removeValue = () => {
+    if(counter>0) setCounter(counter - 1);
+    if(counter>0) setCounter(counter - 1);
+    if(counter>0) setCounter(counter - 1);
     if(counter>0) setCounter(counter - 1);
   }
   return (
